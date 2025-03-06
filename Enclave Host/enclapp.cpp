5a1d675252dd794e420aaa065f466858f38bb904
Atica57 <gjtak578@gmail.com>
2025-03-06 20:38:51 +0900

    modified print statement

--- Enclave Host/enclapp.cpp
+++ Enclave Host/enclapp.cpp
@@ -99,7 +99,7 @@ HRESULT Run()
         printf("Output: %llX\n", reinterpret_cast<ULONG_PTR>(Output));
         printf("Input: %llX\n", Input);
         printf("Output ^ Input: %llX\n", reinterpret_cast<ULONG_PTR>(Output) ^ Input);
-        printf("Finished!");
+        printf("Finished!\n");
     }
 
     // Destructor of "cleanup" variable will terminate and delete the enclave.
